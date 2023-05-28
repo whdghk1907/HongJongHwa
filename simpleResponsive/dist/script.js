987bf4708d8cdf044c2d3b73edc4bdc1029728a9
hong <whdghk1907@naver.com>
2023-05-28 12:44:14 +0900

    자소서 리팩토링

--- simpleResponsive/dist/script.js
+++ simpleResponsive/dist/script.js
@@ -21,7 +21,6 @@ $( document ).ready(function() {
 
   $('#yearAge').text(age);
 
-
   $('.standingHongPhoto').animate({
     'opacity': '1',
   }, {
@@ -51,7 +50,6 @@ $( document ).ready(function() {
 
             // 마우스 오버시 확대 애니메이션 적용
             $(this).mouseenter(function() {
-
               $('<div class="underline"></div>').appendTo($(this));
               $(this).find('.underline').css({
                 'width': 0,
@@ -167,58 +165,4 @@ $( document ).ready(function() {
       developmentIsVisible = true;
     }
   });
-
-  // $(window).scroll( function(){
-  //   var bottom_of_window = $(window).scrollTop() + $(window).height();
-  //
-  //   /* ##### ABOUT MYSELF SECTION #### */
-  //   if( bottom_of_window > ($aboutTitle.offset().top + $aboutTitle.outerHeight())){
-  //     $('.about-myself .content h2').addClass('aboutTitleVisible');
-  //   }
-  // /* ##### EXPERIENCE SECTION #### */
-  //
-  //     // Check the location of each element hidden */
-  //     $('.experience .content .hidden').each( function(i){
-  //
-  //         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-  //
-  //         /* If the object is completely visible in the window, fadeIn it */
-  //         if( bottom_of_window > bottom_of_object ){
-  //
-  //           $(this).animate({
-  //             'opacity':'1',
-  //             'margin-left': '0'
-  //           },600);
-  //         }
-  //     });
-  //
-  // /*###### SKILLS SECTION ######*/
-  //
-  //   var middle_of_developmentWrapper = $developmentWrapper.offset().top + $developmentWrapper.outerHeight()/2;
-  //
-  //   if((bottom_of_window > middle_of_developmentWrapper)&& (developmentIsVisible == false)){
-  //
-  //     $('.skills-bar-container li').each( function(){
-  //
-  //       var $barContainer = $(this).find('.bar-container');
-  //       var dataPercent = parseInt($barContainer.data('percent'));
-  //       var elem = $(this).find('.progressbar');
-  //       var percent = $(this).find('.percent');
-  //       var width = 0;
-  //
-  //       var id = setInterval(frame, 15);
-  //
-  //       function frame() {
-  //         if (width >= dataPercent) {
-  //             clearInterval(id);
-  //         } else {
-  //           width++;
-  //           elem.css("width", width+"%");
-  //           percent.html(width+" %");
-  //         }
-  //       }
-  //     });
-  //     developmentIsVisible = true;
-  //   }
-  // }); // -- End window scroll --
 });
